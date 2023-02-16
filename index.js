f005ad89bccbc984bdb92862a34bb37479478f24
Dageshwar Das <dageshwardasmanikpuri996@gmail.com>
2023-02-16 22:52:48 +0530

    deployement

--- index.js
+++ index.js
@@ -11,20 +11,21 @@ const multer = require('multer');
 const uploadMiddleware = multer({ dest: 'uploads/' });
 const fs = require('fs');
 require('dotenv').config();
-
+const BASE_URL=process.env.BASE_URL
+const MONGODB_URI =process.env.MONGODB_URI
 // MONGODB_URI='mongodb+srv://Dageshwar07:Dagesh0712@cluster0.36e7hn3.mongodb.net/?retryWrites=true&w=majority'
 // BASE_URL='http://localhost:3000'
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-app.use(cors({credentials:true,origin:`${process.env.BASE_URL}`}));
+app.use(cors({credentials:true,origin:`${BASE_URL}`}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 mongoose.set('strictQuery', true);
 
 // mongoose.connect('mongodb+srv://Dageshwar07:Dagesh0712@cluster0.36e7hn3.mongodb.net/?retryWrites=true&w=majority')
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(MONGODB_URI)
 
 app.post('/register', async (req,res) => {
   const {username,password} = req.body;
